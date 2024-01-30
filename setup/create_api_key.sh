401bb4184aaae3bd48a5c022b2eb71b785fa90e0
Raphaël Verdaguer - Lenovo <raphaelverdaguer@me.com>
2024-01-30 18:02:15 +0100

    [add] 8.12 ok + auto update API key

--- setup/create_api_key.sh
+++ setup/create_api_key.sh
@@ -1,21 +1,22 @@
 # Charger les variables d'environnement depuis le fichier .env
-source ../.env
+source .env
 
 CREDS="elastic:"${ELASTIC_PASSWORD}
 
 echo CREDS: ${CREDS}
 
 curl -X GET -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json'
-curl -X DELETE -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json' -d '{"name": "pareto_api_key"}'
+curl -X DELETE -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json' -d '{"name": "ES_PARETO_API_KEY"}'
+curl -X DELETE -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json' -d '{"name": "ES_BACKEND_API_KEY"}'
 curl -X POST -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/*/_clear_cache/?pretty" -H 'Content-Type: application/json'
 
 # Supprimer le fichier API_key.txt s'il existe déjà
 rm -f API_key.txt
 
-# Créer la clé PARETO_API_KEY
+# Créer la clé ES_PARETO_API_KEY
 response=$(curl -X POST -s -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json' -d '
 {
-    "name": "pareto_api_key",
+    "name": "ES_PARETO_API_KEY",
     "role_descriptors": {
         "role-prod": {
             "cluster": ["all"],
@@ -27,16 +28,16 @@ response=$(curl -X POST -s -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_secu
     }
 }')
 
-# Extraire la clé PARETO_API_KEY de la réponse JSON
-pareto_api_key=$(echo "${response}" | grep -oP '(?<="encoded" : ")[^"]*')
+# Extraire la clé ES_PARETO_API_KEY de la réponse JSON
+ES_PARETO_API_KEY=$(echo "${response}" | grep -oP '(?<="encoded" : ")[^"]*')
 
-# Écrire la clé PARETO_API_KEY dans le fichier API_key.txt
-echo "PARETO_API_KEY=${pareto_api_key}" >> API_key.txt
+# Écrire la clé ES_PARETO_API_KEY dans le fichier API_key.txt
+echo "ES_PARETO_API_KEY=${ES_PARETO_API_KEY}" >> API_key.txt
 
-# Créer la clé BACKEND_API_KEY
+# Créer la clé ES_BACKEND_API_KEY
 response=$(curl -X POST -s -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json' -d '
 {
-    "name": "backend_api_key",
+    "name": "ES_BACKEND_API_KEY",
     "role_descriptors": {
         "role-prod": {
             "cluster": ["all"],
@@ -62,13 +63,23 @@ response=$(curl -X POST -s -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_secu
     }
 }')
 
-# Extraire la clé BACKEND_API_KEY de la réponse JSON
-backend_api_key=$(echo "${response}" | grep -oP '(?<="encoded" : ")[^"]*')
+# Extraire la clé ES_BACKEND_API_KEY de la réponse JSON
+ES_BACKEND_API_KEY=$(echo "${response}" | grep -oP '(?<="encoded" : ")[^"]*')
 
-# Écrire la clé BACKEND_API_KEY dans le fichier API_key.txt
-echo "BACKEND_API_KEY=${backend_api_key}" >> API_key.txt
+# Écrire la clé ES_BACKEND_API_KEY dans le fichier API_key.txt
+echo "ES_BACKEND_API_KEY=${ES_BACKEND_API_KEY}" >> API_key.txt
 
 curl -X GET -k -u ${CREDS} "https://${SERVER_IP}:${ES_PORT}/_security/api_key/?pretty" -H 'Content-Type: application/json'
 
 # Afficher le contenu du fichier API_key.txt
 cat API_key.txt
+
+# Remplacer la valeur de ES_PARETO_API_KEY dans le fichier .env
+sed -i "s/^ES_PARETO_API_KEY=.*/ES_PARETO_API_KEY=${ES_PARETO_API_KEY}/" .env
+
+# Remplacer la valeur de ES_BACKEND_API_KEY dans le fichier ../app-ble-backend/.env
+sed -i "s/^ES_BACKEND_API_KEY=.*/ES_BACKEND_API_KEY=${ES_BACKEND_API_KEY}/" ../app-ble-backend/.env
+# Remplacer la valeur de ES_BACKEND_API_KEY dans le fichier ../app-ble-backend/.env.test
+sed -i "s/^ES_BACKEND_API_KEY=.*/ES_BACKEND_API_KEY=${ES_BACKEND_API_KEY}/" ../app-ble-backend/.env.test
+# Remplacer la valeur de ES_BACKEND_API_KEY dans le fichier ../app-ble-backend/.env.development
+sed -i "s/^ES_BACKEND_API_KEY=.*/ES_BACKEND_API_KEY=${ES_BACKEND_API_KEY}/" ../app-ble-backend/.env.development
